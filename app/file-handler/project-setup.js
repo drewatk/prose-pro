5d03ef5fd8cd652ec77c9ef37e6b0705d6557155
vritant24 <vritoku@gmail.com>
2018-09-11 21:21:23 -0400

    created API for creating project structure

---
+++ app/file-handler/project-setup.js
@@ -0,0 +1,47 @@
+/**
+ * Creates a project folder for the given project name
+ * @param {String} projectName 
+ */
+const createProject = (projectName) => {
+    //check if project name can be used
+
+    //create project folder with name as projectName
+
+    //init project
+}
+
+/**
+ * Initializes the project for the given project name
+ * @param {String} projectName 
+ */
+const initProject = (projectName) => {
+
+    //create project.json
+
+    //create project_repo
+        //init git 
+}
+
+/**
+ * Creates a project JSON for the given project name
+ * @param {String} projectName 
+ */
+const createProjectJSON = (projectName) => {
+    //create file
+    
+    //add project name
+
+    //add objects to track branches and versions
+}
+
+/**
+ * Creates the git repo for the given project name
+ * @param {String} projectName 
+ */
+const createGitRepo = (projectName) => {
+    //create folder
+
+    //initialize git in folder
+}
+
+export default createProject;
