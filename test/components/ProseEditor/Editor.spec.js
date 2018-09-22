e0d9c626719bd58f0ddabbccb589cb635714447e
Drew Atkinson <drewatkinson5@gmail.com>
2018-09-22 12:23:14 -0400

    Update Enzyme, add helper function to fill draft text

--- test/components/ProseEditor/Editor.spec.js
+++ test/components/ProseEditor/Editor.spec.js
@@ -1,8 +1,16 @@
 import React from "react";
-import Enzyme, { shallow } from "enzyme";
+import Enzyme, { shallow, mount } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
-import { EditorState } from "draft-js";
+import { ContentState, EditorState } from "draft-js";
 import { ProseEditor } from "../../../app/components/ProseEditor/Editor";
+
+async function fillRTE(component, value) {
+  const input = component.find("DraftEditor");
+
+  input
+    .instance()
+    .update(EditorState.createWithContent(ContentState.createFromText(value)));
+}
 
 Enzyme.configure({ adapter: new Adapter() });
 
@@ -44,5 +52,16 @@ describe("ProseEditor", () => {
     expect(wrapper.find("InlineStyleControls")).toHaveLength(1);
   });
 
-  // TODO: Add tests for inputting into draftjs, investigation needed
+  it("should call onSaveEditorState on update", () => {
+    const wrapper = mount(
+      <ProseEditor
+        editorState={editorState}
+        onSaveEditorState={onSaveEditorState}
+      />
+    );
+
+    fillRTE(wrapper, "foobar");
+
+    expect(onSaveEditorState).toHaveBeenCalled();
+  });
 });
