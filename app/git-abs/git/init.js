4c0344dafa7e0f29565e27743d586dd6172c4f47
vritant24 <vritoku@gmail.com>
2018-09-22 15:57:20 -0400

    add initial commit to new repo

--- app/git-abs/git/init.js
+++ app/git-abs/git/init.js
@@ -1,5 +1,8 @@
 import nodegit from 'nodegit';
 import path from 'path';
+import { gitCons } from 'app/git-abs/constants';
+
+const { AUTHOR, EMAIL, HEAD_REF } = gitCons;
 
 const init = repoPath => {
   const pathToRepo = path.resolve(repoPath);
@@ -7,11 +10,24 @@ const init = repoPath => {
 
   return new Promise((resolve, reject) => {
     nodegit.Repository.init(pathToRepo, isBare)
-      .then(() => {
-        resolve();
-      })
-      .catch(reject(`Error initialising repository in path: ${repoPath}`));
+      .then(repo => createInitialCommit(repo))
+      .then(() => resolve())
+      .catch(err =>
+        reject(`Error initialising repository in path: ${repoPath} \n ${err}`)
+      );
   });
+};
+
+const createInitialCommit = repo => {
+  const author = nodegit.Signature.now(AUTHOR, EMAIL);
+  const committer = nodegit.Signature.now(AUTHOR, EMAIL);
+
+  return repo
+    .refreshIndex()
+    .then(index => index.writeTree())
+    .then(oid =>
+      repo.createCommit(HEAD_REF, author, committer, 'initial commit', oid, [])
+    );
 };
 
 export default init;
