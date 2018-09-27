2d30c7583a8fa339248226b7cca6243c4e10534b
Drew Atkinson <drewatkinson5@gmail.com>
2018-09-27 10:19:18 -0400

    More menu (#13)
    * Add Menu Bar
    
    * Add bootstrap, add File Dropdown
    
    * Fixed Global CSS imports from packages (that took waaaay too long)
    
    * Remove Draft.css
    
    * Draggable title bar on Mac
    
    * Don's show placeholder on windows
    
    * Rename to TitleBar, add Files, history

--- app/containers/HomePage.js
+++ app/containers/HomePage.js
@@ -1,8 +1,15 @@
-import React, { Component } from 'react';
-import Home from 'app/components/Home';
+import React from "react";
+import Home from "app/components/Home";
+import TitleBar from "app/components/TitleBar";
 
-export default class HomePage extends Component {
-  render() {
-    return <Home />;
-  }
-}
+const HomePage = () => {
+  return (
+    <div>
+      <TitleBar />
+      <Home />
+    </div>
+  );
+};
+
+HomePage.displayName = "HomePage";
+export default HomePage;
