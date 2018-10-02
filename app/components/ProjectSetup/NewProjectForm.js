ba973dd9899c56f864227432081986d265f74faf
Drew Atkinson <drewatkinson5@gmail.com>
2018-10-02 15:16:15 -0400

    Fix webpack build error (#27)

--- app/components/ProjectSetup/NewProjectForm.js
+++ app/components/ProjectSetup/NewProjectForm.js
@@ -1,18 +1,19 @@
 import React from "react";
 import { Button, Form, FormGroup, Label, Input } from "reactstrap";
-export default class NewProjectForm extends React.Component {
-  render() {
-    return (
-      <Form>
-        <FormGroup>
-          <Label for="newProjectFormLabel">New Project Form</Label>
-        </FormGroup>
-        <FormGroup>
-          <Label for="projectNameLabel">Project Name</Label>
-          <Input placeholder="Name your project here" />
-          <Button>Create Project</Button>
-        </FormGroup>
-      </Form>
-    );
-  }
-}
+
+const NewProjectForm = () => {
+  return (
+    <Form>
+      <FormGroup>
+        <Label for="newProjectFormLabel">New Project Form</Label>
+      </FormGroup>
+      <FormGroup>
+        <Label for="projectNameLabel">Project Name</Label>
+        <Input placeholder="Name your project here" />
+        <Button>Create Project</Button>
+      </FormGroup>
+    </Form>
+  );
+};
+
+export default NewProjectForm;
