198e07e73958d3bbedc1d54e86d15c88fbcecfad
Drew Atkinson <drewatkinson5@gmail.com>
2018-10-05 16:11:08 -0400

    Expand main window size

--- app/main.dev.js
+++ app/main.dev.js
@@ -61,8 +61,8 @@ app.on("ready", async () => {
 
   mainWindow = new BrowserWindow({
     show: false,
-    width: 1024,
-    height: 728,
+    width: 1400,
+    height: 900,
     titleBarStyle: "hiddenInset"
   });
 
