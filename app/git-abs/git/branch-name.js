33767c8ec9b5c068a0a5a3e86a43635388c87895
Vritant Bhardwaj <vritoku@gmail.com>
2018-10-14 17:19:53 -0400

    changed nodegit version to work with mojave'

--- app/git-abs/git/branch-name.js
+++ app/git-abs/git/branch-name.js
@@ -1,11 +1,9 @@
-import branchName from "current-git-branch";
-
 /**
  * Returns the branch name in the given repo path;
  * @param {String} repoPath
  */
 const getCurrentBranch = repoPath => {
-  const branch = branchName(repoPath);
+  const branch = "";
 
   if (!branch) {
     throw new Error(`No branch found in path: ${repoPath}`);
