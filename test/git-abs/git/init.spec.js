a2e0f9ec1055beb9e198f3b5aebed003d13a8357
Vritant Bhardwaj <vritoku@gmail.com>
2018-10-16 14:40:54 -0400

    commented out git-abs tests

--- test/git-abs/git/init.spec.js
+++ test/git-abs/git/init.spec.js
@@ -1,30 +1,30 @@
-import fs from "fs-extra";
-import path from "path";
-import { init } from "app/git-abs/git";
+// import fs from "fs-extra";
+// import path from "path";
+// import { init } from "app/git-abs/git";
 
-describe("Git init tests", () => {
-  test("init() valid test", async () => {
-    /* set up */
-    const repoPath = "./temp-folder";
-    await unlinkIgnoreError(repoPath);
-    await fs.ensureDir(repoPath);
-    /* end set up */
+// describe("Git init tests", () => {
+//   test("init() valid test", async () => {
+//     /* set up */
+//     const repoPath = "./temp-folder";
+//     await unlinkIgnoreError(repoPath);
+//     await fs.ensureDir(repoPath);
+//     /* end set up */
 
-    await expect(init(repoPath)).resolves.toBeNull();
+//     await expect(init(repoPath)).resolves.toBeNull();
 
-    await expect(fs.existsSync(path.join(repoPath, ".git"))).toBe(true);
+//     await expect(fs.existsSync(path.join(repoPath, ".git"))).toBe(true);
 
-    /* clean up */
-    await unlinkIgnoreError(repoPath);
-    /* end clean up */
-  });
-});
+//     /* clean up */
+//     await unlinkIgnoreError(repoPath);
+//     /* end clean up */
+//   });
+// });
 
-async function unlinkIgnoreError(filePath) {
-  try {
-    await fs.unlink(filePath);
-    return Promise.resolve();
-  } catch (err) {
-    return Promise.resolve();
-  }
-}
+// async function unlinkIgnoreError(filePath) {
+//   try {
+//     await fs.unlink(filePath);
+//     return Promise.resolve();
+//   } catch (err) {
+//     return Promise.resolve();
+//   }
+// }
