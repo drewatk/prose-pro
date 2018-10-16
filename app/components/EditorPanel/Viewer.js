a043708ee0640c7428f94c6418a677983f7e6a24
Drew Atkinson <drewatkinson5@gmail.com>
2018-10-16 19:21:40 -0400

    Remove commented code

--- app/components/EditorPanel/Viewer.js
+++ app/components/EditorPanel/Viewer.js
@@ -2,32 +2,10 @@ import React from "react";
 import { connect } from "react-redux";
 import { stateToHTML } from "draft-js-export-html";
 
-// import styles from "app/components/ProseEditor/ProseEditor.css";
-
 const Viewer = props => {
   const { editorState } = props;
   // TODO: Bring blockquote & code styles from editor
-  // const options = {
-  // blockStyleFn: block => {
-  //   switch (block.getType()) {
-  //     case "blockquote":
-  //       return { attributes: { class: styles.blockquote } };
-  //     default:
-  //       return null;
-  //   }
-  // },
-  // inlineStyles: {
-  //   CODE: {
-  //     element: "span",
-  //     style: {
-  //       backgroundColor: "rgba(0, 0, 0, 0.05)",
-  //       fontFamily: '"Inconsolata", "Menlo", "Consolas", monospace',
-  //       fontSize: 16,
-  //       padding: 2
-  //     }
-  //   }
-  // }
-  // };
+
   const html = stateToHTML(editorState.getCurrentContent());
   return <div dangerouslySetInnerHTML={{ __html: html }} />;
 };
