c99c0e27ef1ccee699709005100dd3fc863a3ef0
Clayton Marshall <marsha88@purdue.edu>
2018-10-26 14:38:33 -0400

    Git Reset Functionality (#54)
    * added reset git functionality
    
    * added reset method to GitAbs class
    
    * made requested changes.

---
+++ app/git-abs/git/reset.js
@@ -0,0 +1,22 @@
+import { Commit, Reset, Oid, CheckoutOptions } from "nodegit";
+
+/**
+ * Performs a hard reset on the branchName inside the repo to the commitHash
+ * @param {Repository (nodegit)} repo
+ * @param {String} branchName
+ * @param {String} commitHash
+ */
+const reset = repo => branchName => commitHash =>
+  Commit.lookup(repo, Oid.fromString(commitHash))
+    .then(commit =>
+      Reset.reset(
+        repo,
+        commit,
+        Reset.TYPE.HARD,
+        new CheckoutOptions(),
+        branchName
+      )
+    )
+    .catch(e => throw new Error(`Error in checkpoint reset ${e}`));
+
+export default reset;
