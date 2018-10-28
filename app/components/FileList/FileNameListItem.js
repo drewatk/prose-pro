b69070564aabf5b2496aedb1245641af2a17d1f3
Drew Atkinson <drewatkinson5@gmail.com>
2018-10-28 14:37:33 -0400

    Delete button

---
+++ app/components/FileList/FileNameListItem.js
@@ -0,0 +1,48 @@
+import React from "react";
+import {
+  ListGroupItem,
+  Dropdown,
+  DropdownToggle,
+  DropdownItem,
+  DropdownMenu
+} from "reactstrap";
+
+class FileNameListItem extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      dropdownOpen: false
+    };
+  }
+
+  toggle() {
+    this.setState(prevState => ({
+      dropdownOpen: !prevState.dropdownOpen
+    }));
+  }
+
+  render() {
+    const { file, onFileItemClick } = this.props;
+    return (
+      <ListGroupItem
+        id="file-item"
+        onClick={() => onFileItemClick(file)}
+        style={{ cursor: "pointer" }}
+      >
+        {file}
+        <Dropdown isOpen={this.state.dropdownOpen} toggle={this.toggle}>
+          <DropdownToggle tag="span">
+            <i className="fas fa-ellipsis-h" style={{ float: "right" }} />
+          </DropdownToggle>
+          <DropdownMenu>
+            <DropdownItem>Delete File</DropdownItem>
+          </DropdownMenu>
+        </Dropdown>
+      </ListGroupItem>
+    );
+  }
+}
+
+export default FileNameListItem;
