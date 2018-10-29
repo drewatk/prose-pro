59e5ee71f1a934ca76af6b4496570bb27ec0d3d9
Drew Atkinson <drewatkinson5@gmail.com>
2018-10-29 16:18:28 -0400

    Wrap Pages in error modal

--- app/containers/ProjectSetupPage.js
+++ app/containers/ProjectSetupPage.js
@@ -5,28 +5,31 @@ import { createProject } from "app/git-abs";
 import TitleBar from "app/components/TitleBar";
 import ProjectSelectionForm from "app/components/Forms/ProjectSelectionForm";
 import NewProjectForm from "app/components/Forms/NewProjectForm";
+import ErrorModal from "app/components/ErrorModal";
 
 import projectSelect from "app/actions/project_selection";
 import { showError } from "app/actions/error";
 
 const ProjectSetupPage = ({ projects, dispatch }) => (
-  <div>
-    <TitleBar />
-    <NewProjectForm
-      onSubmit={({ project }) => {
-        createProject(project)
-          .then(() => dispatch(projectSelect(project)))
-          .catch(err => {
-            console.error(err);
-            dispatch(showError(err.message));
-          });
-      }}
-    />
-    <ProjectSelectionForm
-      projects={projects}
-      onSubmit={({ project }) => dispatch(projectSelect(project))}
-    />
-  </div>
+  <ErrorModal>
+    <div>
+      <TitleBar />
+      <NewProjectForm
+        onSubmit={({ project }) => {
+          createProject(project)
+            .then(() => dispatch(projectSelect(project)))
+            .catch(err => {
+              console.error(err);
+              dispatch(showError(err.message));
+            });
+        }}
+      />
+      <ProjectSelectionForm
+        projects={projects}
+        onSubmit={({ project }) => dispatch(projectSelect(project))}
+      />
+    </div>
+  </ErrorModal>
 );
 
 const mapStateToProps = ({ projects }) => ({ projects });
