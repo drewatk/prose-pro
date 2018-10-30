921651e622fe24dc3cee7484482854f52d7563a4
Drew Atkinson <drewatkinson5@gmail.com>
2018-10-30 18:34:50 -0400

    Update checkpointcard and history tests

--- test/components/History/CheckpointCard.spec.js
+++ test/components/History/CheckpointCard.spec.js
@@ -1,19 +1,19 @@
 import React from "react";
 import Enzyme, { shallow, mount } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
-import CheckpointCard from "app/components/History/CheckpointCard";
+import { CheckpointCard } from "app/components/History/CheckpointCard";
 
 Enzyme.configure({ adapter: new Adapter() });
 
 describe("CheckpointCard", () => {
   it("matches snapshot", () => {
-    const props = { message: "test message", date: 10123 };
+    const props = { version: "test message", timestamp: 10123 };
     const wrapper = shallow(<CheckpointCard {...props} />);
     expect(wrapper).toMatchSnapshot();
   });
 
   it("shows dropdown on click", () => {
-    const props = { message: "test message", date: 10123 };
+    const props = { version: "test message", timestamp: 10123 };
     const wrapper = mount(<CheckpointCard {...props} />);
 
     wrapper.find("i").simulate("click");
