661116f12e703b6c746e7edcabf10cbef0303617
marsha88 <marsha88@purdue.edu>
2018-10-31 12:10:12 -0400

    scaffolding redux setup for cheeckpoint form tests.

--- test/components/Forms/CheckpointForm.spec.js
+++ test/components/Forms/CheckpointForm.spec.js
@@ -1,12 +1,16 @@
 import React from "react";
-import Enzyme, { shallow } from "enzyme";
+import Enzyme, { shallow, mount } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 import { CheckpointForm } from "app/components/Forms/CheckpointForm";
+
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import { reduxForm } from "redux-form";
 
 Enzyme.configure({ adapter: new Adapter() });
 
 describe("Checkpoint Creation Form", () => {
-  it("should should display CheckpointForm", () => {
+  it("should display CheckpointForm", () => {
     const wrapper = shallow(
       <CheckpointForm
         handleSubmit={() => {}}
@@ -15,6 +19,29 @@ describe("Checkpoint Creation Form", () => {
       />
     );
     expect(wrapper.find("Form")).toHaveLength(1);
+  });
+
+  it("should add a checkpoint message and submit", () => {
+    const store = createStore(state => state, { form: {} });
+    const ReduxCheckpointForm = reduxForm({ form: "checkpoint" })(
+      CheckpointForm
+    );
+    const wrapper = mount(
+      <Provider store={store}>
+        <ReduxCheckpointForm
+          handleSubmit={() => {}}
+          pristine={true}
+          submitting={false}
+        />
+      </Provider>
+    );
+
+    expect(wrapper.find("Form")).toHaveLength(1);
+    expect(wrapper.find("Input")).toHaveLength(1);
+    wrapper.find("Input").simulate("change", { target: { value: "foo" } });
+    console.log(store.getState());
+    expect(wrapper.find("button")).toHaveLength(1);
+    wrapper.find("button").simulate("click");
   });
 
   // TODO: Add tests for input manipulation once redux connected to testing.
