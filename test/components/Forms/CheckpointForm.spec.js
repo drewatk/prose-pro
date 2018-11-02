48aced08afcae95783093a7aeaa69be25c37049d
Clayton Marshall <marsha88@purdue.edu>
2018-11-02 14:03:49 -0400

    Checkpoint form tests & History test fixes (#74)
    * scaffolding redux setup for cheeckpoint form tests.
    
    * completed checkpoint form redux integration tests.
    
    * fixed history tests.

--- test/components/Forms/CheckpointForm.spec.js
+++ test/components/Forms/CheckpointForm.spec.js
@@ -1,12 +1,16 @@
 import React from "react";
-import Enzyme, { shallow } from "enzyme";
+import Enzyme, { shallow, mount } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 import { CheckpointForm } from "app/components/Forms/CheckpointForm";
+
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import { reduxForm, reducer as formReducer } from "redux-form";
 
 Enzyme.configure({ adapter: new Adapter() });
 
 describe("Checkpoint Creation Form", () => {
-  it("should should display CheckpointForm", () => {
+  it("should display CheckpointForm", () => {
     const wrapper = shallow(
       <CheckpointForm
         handleSubmit={() => {}}
@@ -17,5 +21,28 @@ describe("Checkpoint Creation Form", () => {
     expect(wrapper.find("Form")).toHaveLength(1);
   });
 
-  // TODO: Add tests for input manipulation once redux connected to testing.
+  it("should add a checkpoint message and submit", () => {
+    const store = createStore(formReducer);
+    const ReduxCheckpointForm = reduxForm({ form: "checkpoint" })(
+      CheckpointForm
+    );
+    const wrapper = mount(
+      <Provider store={store}>
+        <ReduxCheckpointForm onSubmit={() => {}} />
+      </Provider>
+    );
+
+    expect(wrapper.find("Form")).toHaveLength(1);
+    expect(wrapper.find("Input")).toHaveLength(1);
+    expect(wrapper.find("Button")).toHaveLength(1);
+
+    const commitMessage = "Draft 1 Commit";
+    wrapper
+      .find("Input")
+      .simulate("change", { target: { value: commitMessage } });
+    expect(store.getState().checkpoint.values.commitMessage).toBe(
+      commitMessage
+    );
+    wrapper.find("Button").simulate("click");
+  });
 });
--- test/components/History/History.spec.js
+++ test/components/History/History.spec.js
@@ -1,20 +1,44 @@
 import React from "react";
 import Enzyme, { shallow } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
-import { History } from "app/components/History";
+import CheckpointCard from "app/components/History/CheckpointCardTest";
 
 Enzyme.configure({ adapter: new Adapter() });
 
 describe("History View", () => {
-  it("should should display History View", () => {
-    const now = new Date(10101).toLocaleString("en-US", { timeZone: "UTC" });
-    const mockHistory = [
-      { version: "first commit", timestamp: now },
-      { version: "second commit", timestamp: now },
-      { version: "third commit", timestamp: now }
+  it("should display History View", () => {
+    const now = new Date().toLocaleString("en-US", { timeZone: "UTC" });
+    const history = [
+      { version: "first commit", timestamp: now, commit: "<placehold commit>" },
+      {
+        version: "second commit",
+        timestamp: now,
+        commit: "<placehold commit>"
+      },
+      { version: "third commit", timestamp: now, commit: "<placehold commit>" }
     ];
 
-    const wrapper = shallow(<History history={mockHistory} />);
-    expect(wrapper).toMatchSnapshot();
+    const wrapper = shallow(
+      <div>
+        {history.map((data, index) => (
+          <CheckpointCard key={index} {...data} />
+        ))}
+      </div>
+    );
+
+    expect(wrapper.find("CheckpointCard")).toHaveLength(history.length);
+  });
+
+  it("should toggle CheckpointCard options", () => {
+    const now = new Date().toLocaleString("en-US", { timeZone: "UTC" });
+    const checkpoint = {
+      version: "welcome to git",
+      timestamp: now,
+      commit: "12KjkJ4554"
+    };
+    const wrapper = shallow(<CheckpointCard {...checkpoint} />);
+    expect(wrapper.state("dropdownOpen")).toBe(false);
+    wrapper.setState({ dropdownOpen: true });
+    expect(wrapper.find("DropdownItem")).toHaveLength(2);
   });
 });
