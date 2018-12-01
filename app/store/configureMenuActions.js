56b8497f7d32a5ee6ad5318332f46b966963b583
marsha88 <marsha88@purdue.edu>
2018-12-01 15:10:41 -0500

    converting all files to markdown

--- app/store/configureMenuActions.js
+++ app/store/configureMenuActions.js
@@ -1,6 +1,7 @@
-import { convertToRaw } from "draft-js";
+import { convertFromRaw, convertToRaw } from "draft-js";
 import { UPDATE_LAST_SAVED } from "app/actions/history";
 import { ipcRenderer } from "electron";
+import { stateToMarkdown } from "draft-js-export-markdown";
 
 function configureMenuActions(store) {
   ipcRenderer.on("save-file", () => {
@@ -23,12 +24,33 @@ function configureMenuActions(store) {
   });
 
   ipcRenderer.on("export-project", (_, filePath) => {
-    //const { dispatch } = store;
-    const { currentProject, files } = store.getState();
+    const { currentProject, files, gitAbstractions } = store.getState();
     console.log(currentProject, files, filePath);
     if (currentProject) {
-      // iterate through each file and convert it's raw state to plain markdown.
-      // save all files in a new directory within filePath
+      // MAY PROVIDE THIS THING ACCESS TO gitAbstractions FOR EASY FILE PATH INFO.
+      // TODO: MAKE directory fs.ensureDir(filePath + projectName)
+
+      // convert all file's data to raw markdown
+      Promise.all(
+        files.map(
+          file =>
+            new Promise((resolve, reject) => {
+              gitAbstractions
+                .openFile(file)
+                .then(fileData => stateToMarkdown(convertFromRaw(fileData)))
+                .then(mdData => resolve({ name: file, data: mdData }))
+                .catch(err => reject(err));
+            })
+        )
+      )
+        // save all files in a new directory within filePath
+        .then(
+          fileData =>
+            console.log(
+              fileData
+            ) /*Promise.all(fileData.map(({ name, data }) => perform write of data */
+        )
+        .catch(err => console.err("Error in exporting: ", err));
     }
   });
 }
