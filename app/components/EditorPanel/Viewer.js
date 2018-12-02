0f985df37dd0b746ee2877a00e5afb31a20dcf0f
AtbTheGreat <annatiffanybenjamin@gmail.com>
2018-12-02 13:02:44 -0500

    Fixed conflict

--- app/components/EditorPanel/Viewer.js
+++ app/components/EditorPanel/Viewer.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { connect } from "react-redux";
-import { stateToHTML } from "draft-js-export-html";
 
 import { Button } from "reactstrap";
 
@@ -8,43 +7,39 @@ import { EditorState, convertFromRaw } from "draft-js";
 import { UPDATE_EDITOR_STATE, SET_EDIT_STATE } from "app/actions/editor";
 import { showError } from "app/actions/error";
 
-export const Viewer = props => {
-  const { editorState, currentFile, gitAbstractions, dispatch } = props;
-  // TODO: Bring blockquote & code styles from editor
-
-  const html = stateToHTML(editorState.getCurrentContent());
-  return (
-    <div>
-      <Button
-        close
-        style={{ cursor: "pointer", float: "right", marginRight: "50px" }}
-        onClick={() =>
-          gitAbstractions
-            .switchToCurrentVersion(currentFile)
-            .then(fileData =>
-              dispatch({
-                type: UPDATE_EDITOR_STATE,
-                payload: EditorState.createWithContent(convertFromRaw(fileData))
-              })
-            )
-            .then(() => dispatch({ type: SET_EDIT_STATE }))
-            .catch(err => {
-              console.error("Error in exiting checkpoint view: ", err);
-              dispatch(showError(err.message));
-            })
-        }
-      />
-      <div dangerouslySetInnerHTML={{ __html: html }} />
-    </div>
-  );
-};
-
-const mapStateToProps = ({
-  editor: { editorState },
+export const Viewer = ({
+  gitAbstractions,
   currentFile,
-  gitAbstractions
-}) => ({ editorState, currentFile, gitAbstractions });
+  dispatch,
+  children
+}) => (
+  <div>
+    <Button
+      close
+      style={{ cursor: "pointer", float: "right", marginRight: "50px" }}
+      onClick={() =>
+        gitAbstractions
+          .switchToCurrentVersion(currentFile)
+          .then(fileData =>
+            dispatch({
+              type: UPDATE_EDITOR_STATE,
+              payload: EditorState.createWithContent(convertFromRaw(fileData))
+            })
+          )
+          .then(() => dispatch({ type: SET_EDIT_STATE }))
+          .catch(err => {
+            console.error("Error in exiting checkpoint view: ", err);
+            dispatch(showError(err.message));
+          })
+      }
+    />
+    {children}
+  </div>
+);
 
-const WithViewer = connect(mapStateToProps)(Viewer);
+const mapStateToProps = ({ gitAbstractions, currentFile }) => ({
+  gitAbstractions,
+  currentFile
+});
 
-export default WithViewer;
+export default connect(mapStateToProps)(Viewer);
