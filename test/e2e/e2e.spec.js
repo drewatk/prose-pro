63403e1ae5cb9312f1fb818b1b43a634c4965065
Drew Atkinson <drewatkinson5@gmail.com>
2018-12-04 16:30:26 -0500

    More E2E tests

--- test/e2e/e2e.spec.js
+++ test/e2e/e2e.spec.js
@@ -3,6 +3,8 @@ import electronPath from "electron";
 import path from "path";
 import uuid from "uuid/v1";
 import "../../internals/scripts/CheckBuiltsExist";
+import ProjectSetupPage from "./pages/projectSetup";
+import EditorPage from "./pages/editor";
 
 import routes from "app/constants/routes.json";
 
@@ -21,6 +23,8 @@ describe("E2E", function spec() {
   });
 
   afterEach(async () => {
+    await checkForConsoleErrors(this.app.client);
+
     if (this.app && (await this.app.isRunning())) {
       return await this.app.stop();
     }
@@ -34,23 +38,17 @@ describe("E2E", function spec() {
       await delay(500);
       const title = await browserWindow.getTitle();
       expect(title).toBe("ProsePro");
-
-      await checkForConsoleErrors(client);
     });
   });
 
   describe("Projects", () => {
     it("should create a project", async () => {
       const { client } = this.app;
+      const projectPage = new ProjectSetupPage(this.app);
 
       await client.waitUntilWindowLoaded();
 
-      await client.setValue(
-        "[data-test-id='project-name-field'] input",
-        `Test Project: ${uuid()}`
-      );
-
-      await client.click("[data-test-id='create-project-button']");
+      await projectPage.newProject(`test_project_${uuid()}`);
 
       await delay(500);
       await client.waitUntilWindowLoaded();
@@ -58,33 +56,48 @@ describe("E2E", function spec() {
       const url = await client.getUrl();
 
       expect(url.endsWith(routes.EDITOR)).toBeTruthy();
-
-      await checkForConsoleErrors(client);
     });
 
-    it("open an existing project", async () => {
+    it("opens an existing project", async () => {
       const { client } = this.app;
+      const projectPage = new ProjectSetupPage(this.app);
+
       await client.waitUntilWindowLoaded();
 
-      await client.selectByIndex("[data-test-id='project-select-field']", 1);
-
-      await client.click("[data-test-id='project-select-button']");
-
-      await delay(500);
-      await client.waitUntilWindowLoaded();
+      await projectPage.selectProject(1);
 
       const url = await client.getUrl();
 
       expect(url.endsWith(routes.EDITOR)).toBeTruthy();
+    });
+  });
 
-      await checkForConsoleErrors(client);
+  describe("Editor", () => {
+    it("Enters Text into the editor", async () => {
+      const text =
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque posuere laoreet tristique. Cras ac lacinia nulla, sit amet ultrices libero. Duis sit amet placerat erat. Sed leo massa, semper non ipsum sit amet, ullamcorper consequat eros. Etiam enim neque, mattis ac est in, vehicula tempor lacus. Cras ac porta quam, a porta sem. Vivamus vitae tincidunt eros. Nam sed scelerisque nisl. Mauris semper a nisi et ultricies. Sed quis mi pretium, commodo sapien vitae, condimentum risus. Sed vitae ante iaculis, elementum velit vitae, pretium turpis. Duis aliquam mauris ut arcu egestas, nec venenatis nunc ornare. Integer id est a enim porta rutrum a et mauris. In quis neque nec mauris viverra gravida vitae non dui.";
+
+      const { client } = this.app;
+      const projectPage = new ProjectSetupPage(this.app);
+      const editorPage = new EditorPage(this.app);
+      await client.waitUntilWindowLoaded();
+
+      await projectPage.selectProject(1);
+
+      await editorPage.newFile(`test_file_${uuid()}`);
+
+      await editorPage.type(text);
+
+      await editorPage.save();
+
+      expect(await editorPage.hasContent(text)).toBeTruthy();
     });
   });
 });
 
 const checkForConsoleErrors = async client => {
   const logs = await client.getRenderProcessLogs();
-  // Print renderer process logs
+  // Print render er process logs
   logs.forEach(log => {
     console.log(log.message);
     console.log(log.source);
