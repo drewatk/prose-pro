1f59310ab53af5d345b1e44a72c2e03115a55d9a
marsha88 <marsha88@purdue.edu>
2018-12-05 15:23:54 -0500

    mutation is a real bitch

--- app/components/History/index.js
+++ app/components/History/index.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { connect } from "react-redux";
+import * as R from "ramda";
 
 import CheckpointCard from "./CheckpointCard";
 
 export const History = ({ history }) => (
   // TODO: Move styles to CSS file
   <div style={{ height: "100%" }}>
-    {history.reverse().map((data, index) => (
+    {R.reverse(history).map((data, index) => (
       <CheckpointCard key={index} {...data} />
     ))}
   </div>
