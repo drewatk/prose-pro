3c71e050fe4556f3a54edb6f9c90c0238ff14f2c
Drew Atkinson <drewatkinson5@gmail.com>
2018-12-05 17:14:03 -0500

    User friendly date on checkpoint card (#103)
    * User friendly date on checkpoint card
    
    * Mock Moment

--- test/components/History/CheckpointCard.spec.js
+++ test/components/History/CheckpointCard.spec.js
@@ -2,6 +2,13 @@ import React from "react";
 import Enzyme, { shallow, mount } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 import { CheckpointCard } from "app/components/History/CheckpointCard";
+import moment from "moment";
+
+jest.mock("moment");
+
+moment.mockImplementation(timestamp => ({
+  fromNow: jest.fn(() => `moment_mock_fromNow_${timestamp}`)
+}));
 
 Enzyme.configure({ adapter: new Adapter() });
 
